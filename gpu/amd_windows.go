73c49d57e89760b817a3dbd17d37ca32aef596e0
likelovewant <crazylove63212@gmail.com>
2024-05-24 20:06:28 +0800

    Update amd_windows.go
    remove this will broken the installer build

--- gpu/amd_windows.go
+++ gpu/amd_windows.go
@@ -108,10 +108,10 @@ func AMDGetGPUInfo() []GpuInfo {
 		}
 
 		// iGPU detection, remove this check once we can support an iGPU variant of the rocm library
-		//if totalMemory < IGPUMemLimit {
-		//	slog.Info("amdgpu appears to be an iGPU, skipping", "gpu", i, "total", format.HumanBytes2(totalMemory))
-		//	continue
-		//}
+		if totalMemory < IGPUMemLimit {
+			slog.Info("amdgpu appears to be an iGPU, skipping", "gpu", i, "total", format.HumanBytes2(totalMemory))
+			continue
+		}
 
 		// TODO revisit this once ROCm v6 is available on windows.
 		// v5.7 only reports VRAM used by this process, so it's completely wrong and unusable
